cfdd1099a1b0864943934270b915bb0b84afa8fb
Steven <144975699+stevendotexe@users.noreply.github.com>
2024-11-12 01:37:11 +0700

    Destino OSM

---
+++ script.js
@@ -0,0 +1,26 @@
+var map = L.map('map').setView([-6.9175, 107.6191], 12);
+L.tileLayer('https://api.maptiler.com/maps/basic-v2-dark/{z}/{x}/{y}.png?key=KP8XxRPzOszUwLbNmQud', {
+    maxZoom: 18,
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+}).addTo(map);
+
+fetch('busLinesData.geojson').then(response => response.json()).then(data => {
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: 5,  // Make points smaller
+                color: "red"
+            });
+        },
+        style: {
+            color: "white",
+            weight: 4
+        },
+        onEachFeature: function (feature, layer){
+            if (feature.properties && feature.properties.name){
+                layer.bindPopup("Route: " + feature.properties.name);
+            }
+        }
+    }).addTo(map);
+})
+.catch(error => console.log('Error loading GeoJSON data:', error));
