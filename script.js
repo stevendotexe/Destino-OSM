475a3e66a72837106da042e4a4fee08912d91865
stevendotexe <144975699+stevendotexe@users.noreply.github.com>
2024-11-13 07:55:51 +0700

    bus stops names corridor 1 & 5 completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([-6.9175, 107.6191], 12);
 L.tileLayer('https://api.maptiler.com/maps/basic-v2-dark/{z}/{x}/{y}.png?key=KP8XxRPzOszUwLbNmQud', {
-    maxZoom: 18,
+    maxZoom: 23,
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 }).addTo(map);
 
@@ -21,14 +21,14 @@ fetch('busLinesData.geojson').then(response => response.json()).then(data => {
 
             pointToLayer: function(feature, latlng) {
                 const marker = L.circleMarker(latlng, {
-                    radius: 1,
+                    radius: 2,
                     color: "white"
                 });
                 
                 if(feature.properties["@relations"][0]["stop_name"]){
                     marker.bindPopup(feature.properties["@relations"][0]["stop_name"]);
                 } else if (feature.properties["@relations"][0]["role"] === "platform") {
-                    return;
+                    marker.bindPopup("PLATFORM " + feature.properties["@id"]); 
                 }
                 else {
                     marker.bindPopup(feature.properties["@id"]); // CHANGE TO "STOP LATER"
